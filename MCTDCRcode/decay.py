2068740c00c8928833fa8819de268ca9d6e2aa9d
RomainCoulon <64598456+RomainCoulon@users.noreply.github.com>
2023-02-10 11:30:44 +0000

    Test to save the decay chain diagram

--- MCTDCRcode/decay.py
+++ MCTDCRcode/decay.py
@@ -1,6 +1,12 @@
+# pip install radioactivedecay
 import radioactivedecay as rd
+import matplotlib.pyplot as plt
 
 Ac225_t0 = rd.Inventory({'Ac-225': 1}, 'Bq')
 Ac225_t1 = Ac225_t0.decay(2, 'd')
 Ac225_t1.activities('Bq')
 print(Ac225_t1)
+
+nuc = rd.Nuclide('Ac-225')
+decayChain = nuc.plot()
+plt.savefig('decayChain.png')
