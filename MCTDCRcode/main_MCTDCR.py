c6a2be9305b69e6a9a95b6a4c665fb171adfe173
RomainCoulon <64598456+RomainCoulon@users.noreply.github.com>
2023-03-01 15:29:11 +0000

    Discussed with Jialin, commented to define the work plan

--- MCTDCRcode/main_MCTDCR.py
+++ MCTDCRcode/main_MCTDCR.py
@@ -16,22 +16,23 @@ import scipy.stats as st
 
 ## INPUT OF THE MODEL
 
-N=1000 # number of simulated decay (MC trials)
+N=100 # number of simulated decay (MC trials)
 
 
-Rad=["H-3", "Co-60"]    # list of radionuclides
-pmf_1=[0.10, 1]         # relative abondance (pmf)
+Rad=["H-3", "Co-60"]       # list of radionuclides
+pmf_1=[0.10, 0.91]         # relative abondance (pmf)
+if sum(pmf_1 != 1): print("warning p not equal to 1")
 
 kB = 0.01                  # Birks constant in cm/MeV
 L = 0.0056                  # the free parameter /MeV-1
 
 TDCR_measure = 0.977784        # Measured TDCR value
-u_TDCR_measure = 0.000711
+u_TDCR_measure = 0.000711      # standard uncertainty
 
 ## RUN THE MC CALCULATION
-efficiency_D = []
-efficiency_T = []
-tdcr = []
+efficiency_D = []        # results calculated efficiency for double coincidence
+efficiency_T = []        # results calculated efficiency for triple coincidence
+tdcr = []                # results of ratio of calculated efficiencies
 
 for i in range(N): # Main Loop
     particle_vec=[]
@@ -44,105 +45,123 @@ for i in range(N): # Main Loop
     
     # Read PenNuc File
     out_PenNuc = tl.readPenNuc(rad_i)
-    particle = out_PenNuc[0]
-    p_branch = out_PenNuc[1]
-    e_branch = out_PenNuc[2]
-    LevelDaughter = out_PenNuc[3]
-    levelNumber = out_PenNuc[4]
-    prob = out_PenNuc[5]
-    levelEnergy = out_PenNuc[6]
-    transitionType = out_PenNuc[7]
-    e_trans = out_PenNuc[8] 
-    next_level = out_PenNuc[9] 
-    Q_value = out_PenNuc[10]
+    particle = out_PenNuc[0]          # Particle(s) from the Mother 
+    p_branch = out_PenNuc[1]          # Probablity of the branch
+    e_branch = out_PenNuc[2]          # Energy of the branch
+    LevelDaughter = out_PenNuc[3]     # Level of the Daughter nucleus just after the particle emission
+    levelNumber = out_PenNuc[4]       # The vector of level of the daughter to get information of all possible isomeric transitions
+    prob = out_PenNuc[5]              # Probibility density for each of the daughter level
+    levelEnergy = out_PenNuc[6]       # Energy of each level
+    transitionType = out_PenNuc[7]    # type of each possible transitions (internal transitions or gamma emission)
+    e_trans = out_PenNuc[8]           # Energy of the transition
+    next_level = out_PenNuc[9]        # Next level on the daughter nucleus
+    Q_value = out_PenNuc[10]          # Energy of the reaction
     
     ## sampling of the decay branch
     multiplicity_branch = sum(np.asarray(p_branch))
     index_branch = tl.sampling(p_branch)
-    particle_branch = particle[index_branch]
-    energy_branch =  e_branch[index_branch]
-    probability_branch = p_branch[index_branch]
-    levelOftheDaughter = LevelDaughter[index_branch]
+    particle_branch = particle[index_branch]            # sampled particle emitted by the mother
+    energy_branch =  e_branch[index_branch]             # energy of the particle emitted by the mother
+    probability_branch = p_branch[index_branch]         # probability of the sampled branch
+    levelOftheDaughter = LevelDaughter[index_branch]    # Level of the daughter just after the particle emission from the mother
     print("\t Sampled decay branch:")
     print("\t\t Particle = ", particle_branch)
     print("\t\t Energy of the particle = ", energy_branch, " keV")
     print("\t\t Level of the daughter nucleus = ", levelOftheDaughter)
 
     # Scoring
-    e_sum = energy_branch # Energy summary
-    particle_vec.append(particle_branch)
-    energy_vec.append(energy_branch)
+    e_sum = energy_branch                               # Update the Energy summary
+    particle_vec.append(particle_branch)                # Update of the particle vector
+    energy_vec.append(energy_branch)                    # Update of the energy of the particle
     
-    print("\t Subsequent isomeric transition")
-    while levelOftheDaughter > 0:
-      i_level = levelNumber.index(levelOftheDaughter)
+    print("\t Subsequent isomeric transition")          # finish with the mother / now with the daughter
+    while levelOftheDaughter > 0:                       # Go on the loop while the daughter nucleus is a its fundamental level (energy 0)
+      i_level = levelNumber.index(levelOftheDaughter)   # Find the position in the daughter level vector 
       ## sampling of the transition in energy levels of the daughter nucleus
-      index_t = tl.sampling(prob[i_level+1])
+      index_t = tl.sampling(prob[i_level+1])            # sampling of the transition
       print("\t\t ***")
       print("\t\t Energy of the level = ", levelEnergy[i_level], " keV")
       print("\t\t Transition type = ", transitionType[i_level+1][index_t])
       print("\t\t Energy of the transition = ", e_trans[i_level+1][index_t], "keV")
       print("\t\t next level = ", next_level[i_level+1][index_t])
-      levelOftheDaughter = next_level[i_level+1][index_t]
-      
+            
       # Scoring
-      if transitionType[i_level+1][index_t] == "GA":
-          particle_vec.append("gamma")
-      else:
-          particle_vec.append("electron")
-      energy_vec.append(e_trans[i_level+1][index_t])
-      e_sum += e_trans[i_level+1][index_t] # Energy summary
+      if transitionType[i_level+1][index_t] == "GA": # if it is a gamma that has been emitted
+          particle_vec.append("gamma")               # Update of the particle vector
+      else:                                          # if not, it is a internal conversion, so an electron
+          particle_vec.append("electron")               # !!!!!!!!! it is OK for our model? Does the electron leave with the kinetic enegy of the transition 
+      energy_vec.append(e_trans[i_level+1][index_t])    # Update the energy vector
+      e_sum += e_trans[i_level+1][index_t]              # Energy summary
+
+      levelOftheDaughter = next_level[i_level+1][index_t]   # set the next level
     
-    
+    # Finish with the daughter Nucleus
     print("\t Summary of the nuclear decay")
     print("\t\t particles : ", particle_vec)
     print("\t\t energy : ", energy_vec, "keV")
     print("\t\t remaing energy (atomic transitions) : ", Q_value-e_sum, " keV")
 
-    ## Atomic transitions to develop...!!!!!!!!!!
+    ## But we have not finish with the daughter atom
+    ## !!!!!!!!!! We need to develop the atomic transitions...
+    ## to produce x-rays and Auger electrons
+    ## Probably in link with the internal conversion EK, EL, EM (voir variable transitionType)
+    ## You can try to see how we can use data from file".lara.txt"  (L icon on LNHB web site)
+    ## We can trick/simply at first time
     
     
-    ## Calculation of deposited energy
-    
+    ## Calculation of deposited energy in the liquid source in the vial
+    # For alpha particle - do nothing / we consider that all the energy is release in the solution
+    # For electrons (Auger and internal conversion) - do nothing / we consider that all the energy is release in the solution
+    # !!!!!! For beta - sampling into the spectrum / use the BetaShape sprectrum on the LNHB web site
+    # make a new funtion to replace the readBetaSpectrum() in order to directly get the spectrum from the web
+    # be careful with the energy unit
+    # !!!!!! For gamma and x - Develop a model in MCNP6 (LANL)
+    # make a Python code to produce the source code de MCNP6 and to read the output to contruct the Matrix (incident energy, deposited energy)
+    # Once we have the matrix, build a function to sample in the matrix given E + interpolation feature
     for i, p in enumerate(particle_vec):
         if p == "beta":
             e_beta, p_beta, n_bin = tl.readBetaSpectrum(rad_i)
             index_beta_energy = tl.sampling(p_beta)
             particle_vec[i] = "electron"
             energy_vec[i] = e_beta[index_beta_energy]*1e-3
-        if p == "gamma":
+        if p == "gamma" or p == "x":
             particle_vec[i] = "electron" # false Compton scattering... to develop...!!!!!!!!!!!!!!
                
     print("\t Summary of the final charged particles")
     print("\t\t particles : ", particle_vec)
     print("\t\t energy : ", energy_vec, "keV")
  
-    ## Calculation of the scintillation quenching
-    
+    ## Now we have the (particle, energy) vectors that we would like
+
+    ## Calculation of the scintillation quenching with the Birks Model
     for i, p in enumerate(particle_vec):
         if p == "alpha":
-            energy_vec[i] = energy_vec[i]/(1+kB*tl.stoppingpowerA(energy_vec[i]*1e3)) # false to correct
+            energy_vec[i] = energy_vec[i]/(1+kB*tl.stoppingpowerA(energy_vec[i]*1e3))
+            #!!!!!!! Develop the function stoppingpowerA() to calculate the stopping power for alpha particles 
         if p == "electron":
             energy_vec[i] = energy_vec[i]/(1+kB*tl.stoppingpowerE(energy_vec[i]*1e3))
     
     print("\t\t quenched energy : ", energy_vec, "keV")
     
-    ## Calculation of the TDCR ratio
+    ## Calculation of the TDCR ratio 
+    ## We fill our 3 results vectors
     efficiency_T.append((1-np.exp(-L*sum(energy_vec)/3))**3)
     efficiency_D.append(3*(1-np.exp(-L*sum(energy_vec)/3))**2-2*efficiency_T[-1])
     tdcr.append(efficiency_T[-1]/efficiency_D[-1])
 
-mean_efficiency_T = np.mean(efficiency_T)
-std_efficiency_T = np.std(efficiency_T)
+
+# We calculate the final estimator
+mean_efficiency_T = np.mean(efficiency_T) # average
+std_efficiency_T = np.std(efficiency_T)   # standard deviation
 mean_efficiency_D = np.mean(efficiency_D)
 std_efficiency_D = np.std(efficiency_D)
 mean_TDCR = np.mean(tdcr)
 std_TDCR = np.std(tdcr)
 #std_TDCR_calcul = TDCR_calcul*np.sqrt((std_efficiency_D/mean_efficiency_D)**2+(std_efficiency_T/mean_efficiency_T)**2)/np.sqrt(N) # no correlation false !!!
 
-residual = (mean_TDCR-TDCR_measure)**2 # Calculation of the residual value to minimize
-u_residual = np.sqrt(4*(mean_TDCR+TDCR_measure)**2*(std_TDCR**2+u_TDCR_measure**2))
-Z_test = abs(mean_TDCR-TDCR_measure)/np.sqrt(std_TDCR**2+u_TDCR_measure**2)
+residual = (mean_TDCR-TDCR_measure)**2 # Calculation of the residual value to minimize to find the best free parameter L
+u_residual = np.sqrt(4*(mean_TDCR+TDCR_measure)**2*(std_TDCR**2+u_TDCR_measure**2))  # uncertainty of the residual 
+Z_test = abs(mean_TDCR-TDCR_measure)/np.sqrt(std_TDCR**2+u_TDCR_measure**2)          # statisitcal test to see if we can improve the estimation
 
 print("\t TDCR calculation")
 print("\t\t Efficiency of Triple coincident events : ", round(100*mean_efficiency_T,3), "+/-", round(100*std_efficiency_T,3), " %")
@@ -152,6 +171,8 @@ print("\t\t Measured TDCR value : ", round(TDCR_measure,5), "+/-", round(u_TDCR_
 print("\t\t Residual : ", residual, '+/-', u_residual)
 print("Z_test = ", Z_test)
     
+
+# to discuss the meaning of this for the measurement 
 
 x = np.arange(np.mean(efficiency_T),1.001,0.001)
 plt.figure("efficiency distribution")
