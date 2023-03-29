09474a541ae9eeffc6032d9189a96790e2bea9d9
JialinHu98 <124688873+JialinHu98@users.noreply.github.com>
2023-03-29 13:32:20 +0000

    redéposer les résultats du MCNP

--- MCTDCRcode/MCNP-MATRIX/test.py
+++ MCTDCRcode/MCNP-MATRIX/test.py
@@ -7,7 +7,7 @@ end_E = 20                               # end for the deposited energy / keV
 delta_E= 0.2                             # the step the distribution / keV
 NPS = 1e5 
 
-
+'''
 with open("input/input_1MeV.mcn",mode='w') as file:
     for i in range(1,44):
         a = lc.getline('input/template.txt',i)
@@ -19,6 +19,8 @@ with open("input/input_1MeV.mcn",mode='w') as file:
         file.write(a)
     file.write("E18:P,E "+str(0)+" "+str(int(100))+"i "+str(1)+"       $ binning of energy\n") # line 49
     file.write("NPS "+str(int(1e7))+"          $ number of source particles") # line 50
+'''
+#with open("script.bat", "w") as file2:
+    #file2.write("mcnp6 i=input/input_"+str(int(1))+"MeV.mcn r=run/run_"+str(int(1))+"MeV.r o=output/output_"+str(int(1))+"MeV.o\n")
 
-with open("script.bat", "w") as file2:
-    file2.write("mcnp6 i=input/input_"+str(int(1))+"MeV.mcn r=run/run_"+str(int(1))+"MeV.r o=output/output_"+str(int(1))+"MeV.o\n")
+f = open("output/output_"+"keV.txt",'w')
