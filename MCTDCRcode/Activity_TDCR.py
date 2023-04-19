bd97e797ad39794b9d08a3ee80ab31dfa71c85f2
Romain Coulon <romain.coulon@bipm.org>
2023-04-19 09:44:52 +0200

    test activity estimation for H-3

--- MCTDCRcode/Activity_TDCR.py
+++ MCTDCRcode/Activity_TDCR.py
@@ -13,55 +13,68 @@ import scipy.optimize as opt
 def readEff(Rad, kB, SDT):
     file = open("EfficiencyCurves/"+''.join(Rad)+"/Eff"+SDT+"_"+''.join(Rad)+'_[1]_'+str(kB)+".txt","r")
     L=[]
-    pS=[]
-    upS=[]
+    p=[]
+    up=[]
     for row in file:
         H=row.split(" ")
         L.append(float(H[0]))
-        pS.append(float(H[1]))
-        upS.append(float(H[2]))
-    return L, pS, upS
-
-def eff0(L, Lv, pSv):
-    pS = np.interp(L, Lv, pSv)
-    pT  = pS**3
-    pD  = 3*pS**2-2*pT
-    return pT, pD
-
-def res0(L, TD, Lv, pSv):
-    return (eff0(L, Lv, pSv)[0]/eff0(L, Lv, pSv)[1]-T/D)**2
-
-def eff(L, Lv, pSv):
-    pSA = np.interp(L[0], Lv, pSv)
-    pSB = np.interp(L[1], Lv, pSv)
-    pSC = np.interp(L[2], Lv, pSv)
-    pAB = pSA*pSB
-    pBC = pSB*pSC
-    pAC = pSA*pSC
-    pT  = pSA*pSB*pSC
-    pD  = pAB+pBC+pAC-2*pT
-    return pT, pAB, pBC, pAC, pD
-
-def res(L, TAB, TBC, TAC, Lv, pSv):
-    a = eff(L, Lv, pSv)
-    return (a[0]/a[1]-TAB)**2+(a[0]/a[2]-TBC)**2+(a[0]/a[3]-TAB)**2
+        p.append(float(H[1]))
+        up.append(float(H[2]))
+    return L, p, up
 
 def readProfil(Rad,kB,SDT):
     Lv, pSv, upSv = readEff(Rad, kB, SDT)
     pSv = sg.savgol_filter(pSv, 11, 3)
     return Lv, np.asarray(pSv), upSv
 
-def effTDCR(TD, TAB, TBC, TAC, Rad, kB):
-    Lv, pSv, _ = readProfil(Rad,kB,"D")
-    # Lv, pSv, _ = readProfil(Rad,0.8e-5,"D")
-    # Lv, pSv, _ = readProfil(Rad,0.9e-5,"D")
-    # Lv, pSv, _ = readProfil(Rad,1.0e-5,"D")
-    L = 0.5
-    r0 = opt.minimize_scalar(res0, args=(TD, Lv, pSv), method='golden')
-    L0 = (r0.x, r0.x, r0.x)
-    # r = opt.minimize(res, L0, args=(TAB, TBC, TAC, Lv, pSv), method='nelder-mead',options={'xatol': 1e-7, 'disp': True, 'maxiter':400})
-    # L=r.x[:3]
-    return L0[0], eff0(L0[0], Lv, pSv)[1]
+def res(L, TD, Lv, pDv, pTv):
+    return (np.interp(L, Lv, pTv)/np.interp(L, Lv, pDv)-T/D)**2
+
+def effTDCR(TD, Rad, kB):    
+    kBv = [0.8e-5, 0.9e-5, 1.0e-5, 1.1e-5, 1.2e-5]
+    Lv0 = readProfil(Rad,0.8e-5,"D")[0]
+    pDv=[]; pTv=[]; Lv1=[]; effD1=[]; effT1=[]
+    for i in kBv:
+        pDv.append(readProfil(Rad,i,"D")[1])
+        pTv.append(readProfil(Rad,i,"T")[1])
+        Lv1.append(opt.minimize_scalar(res, args=(TD, Lv0, pDv[-1], pTv[-1]), method='golden').x)
+        effD1.append(np.interp(Lv1[-1], Lv0, pDv[-1]))
+        effT1.append(np.interp(Lv1[-1], Lv0, pTv[-1]))
+    L = np.interp(kB, kBv, Lv1)
+    effD = np.interp(kB, kBv, effD1)
+    effT = np.interp(kB, kBv, effT1)
+     
+    plt.figure("eff_D = f(kB)")
+    plt.clf()
+    plt.title('  ')
+    plt.plot(kBv, effD1,'-b')
+    plt.plot(kB, effD, 'or')
+    plt.xlabel(r"$kB$ /(cm keV$^{-1}$)", fontsize = 14)
+    plt.ylabel(r"$\epsilon_D$", fontsize = 14)
+    plt.legend(fontsize = 12)
+    # plt.close()
+    
+    plt.figure("eff_T = f(kB)")
+    plt.clf()
+    plt.title('  ')
+    plt.plot(kBv, effT1,'-b')
+    plt.plot(kB, effT, 'or')
+    plt.xlabel(r"$kB$ /(cm keV$^{-1}$)", fontsize = 14)
+    plt.ylabel(r"$\epsilon_T$", fontsize = 14)
+    plt.legend(fontsize = 12)
+    # plt.close()
+    
+    plt.figure("L = f(kB)")
+    plt.clf()
+    plt.title('  ')
+    plt.plot(kBv, Lv1,'-b')
+    plt.plot(kB, L, 'or')
+    plt.xlabel(r"$kB$ /(cm keV$^{-1}$)", fontsize = 14)
+    plt.ylabel(r"$L$ /keV$^{-1}$", fontsize = 14)
+    plt.legend(fontsize = 12)
+    # plt.close()
+    
+    return L, effD, effT
 
 def plotEffProfil(Rad,kB):
     LS, effS, _ = readProfil(Rad,kB,"S")
@@ -98,17 +111,17 @@ BC = 695.919
 AC = 693.579
 T = 448.868
 D = AB+BC+AC-2*T
-out = effTDCR(T/D, T/AB, T/BC, T/AC, "H-3", 1.0E-5) # H-3
+out = effTDCR(T/D, "H-3", 1.05E-5) # H-3
+plotEffProfil("H-3", 1.00E-5)
 # out = effTDCR(657.296, 695.919, 693.579, 448.868) # Co-60
 
 digRound = 2
 print("Efficiency of double coincidences = ", round(100*out[1], digRound),"%")
-# print("Efficiency of triple coincidences = ", round(100*out[4], digRound),"%")
+print("Efficiency of triple coincidences = ", round(100*out[2], digRound),"%")
 print("Activity from double coincidences = ", round(D/out[1], digRound), "Bq")
-# print("Activity of triple coincidences = ", round(T/out[4], digRound), "Bq")
+print("Activity of triple coincidences = ", round(T/out[2], digRound), "Bq")
 # print("Efficiency of double coincidences (asym) = ", round(100*out[3], digRound),"%")
 # print("Efficiency of triple coincidences (asym) = ", round(100*out[5], digRound),"%")
 # print("Activity from double coincidences (asym) = ", round(D/out[3], digRound), "Bq")
 # print("Activity of triple coincidences (asym) = ", round(T/out[5], digRound), "Bq")
 
-plotEffProfil("H-3",1.0E-5)
