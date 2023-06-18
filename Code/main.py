76d317973090efad1822c14dc8e8188e6c34afb3
Romain Coulon <romain.coulon@bipm.org>
2023-06-18 12:46:40 +0200

    C-14 done

--- Code/main.py
+++ Code/main.py
@@ -18,7 +18,7 @@ import scipy.stats as st
 ## INPUT OF THE MODEL
 # N=1                   # number of simulated decay (MC trials)
 N= 10000
-Rad=["C-11"]            # list of radionuclides (Na-24)
+Rad=["C-14"]            # list of radionuclides (Na-24)
 # Rad = ["Cs-137"]
 pmf_1=[1]                # relative abondance (pmf)
 # kB =[1.0e-5]
