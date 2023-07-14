a0ae4a001d0e148e1653ae404d3faf4fdadb1b63
Romain Coulon <romain.coulon@bipm.org>
2023-07-14 15:58:55 +0200

    update distribution

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = "0.0.5"
+VERSION = "0.0.6"
 DESCRIPTION = "TDCR model"
 
 setup(
@@ -12,15 +12,18 @@ setup(
     author_email = "<romain.coulon@bipm.org>",
     description = DESCRIPTION,
     packages = find_packages(),
-    install_requires = ["numpy"],
+    install_requires = ["numpy","scipy","sys","time","urllib","zipfile","re"],
     keywords = ["python","TDCR","Monte-Carlo","radionuclide","scintillation","counting"],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Science/Research",
 		"License :: OSI Approved :: MIT License",
+		"Natural Language :: English",
+		"Natural Language :: French",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
+		"Topic :: Scientific/Engineering :: Physics",
     ]
 )
