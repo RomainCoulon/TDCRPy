70dd9a7c2224b5a06a864a5a33575eb890d920bf
Romain Coulon <romain.coulon@bipm.org>
2023-07-19 14:55:53 +0200

    update distributed version alpha

--- Code/__init__.py
+++
@@ -1,3 +0,0 @@
-from TDCRPy import TDCRPy
-from TDCRPy import TDCR_model_lib
-from TDCRPy import TDCRoptimize
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = "0.0.8"
+VERSION = "0.0.29"
 DESCRIPTION = "TDCR model"
 
 with open("README.md", "r") as f:
@@ -18,18 +18,27 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/RomainCoulon/TDCRPy",
     packages = find_packages(),
-    install_requires = ["numpy","scipy","sys","time","urllib","zipfile","re"],
+    install_requires = ["numpy"],
     keywords = ["python","TDCR","Monte-Carlo","radionuclide","scintillation","counting"],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Science/Research",
-	"License :: OSI Approved :: MIT License",
-	"Natural Language :: English",
-	"Natural Language :: French",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Natural Language :: French",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-	"Topic :: Scientific/Engineering :: Physics",
-    ]
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    include_package_data = True,
+    package_data = {'': [
+	    'decayData/All-nuclides_PenNuc.zip',
+        'decayData/All-nuclides_BetaShape.zip',
+		'decayData/All-nuclides_Ensdf.zip',
+        'Quenching/alpha_toulene.txt',
+		'Quenching/TandataUG.txt',
+		'MCNP-MATRIX/matrice/fichier/*.txt',
+		]},
 )
