5c4856a2c7a1466a507587d4b609878f174688c9
Jialin HU <jialin.hu@bipm.org>
2023-10-05 14:12:28 +0200

    tracer matrice 2000-10000keV

--- tdcrpy/MCNP-MATRIX/matrice.py
+++ tdcrpy/MCNP-MATRIX/matrice.py
@@ -496,16 +496,16 @@ def find_info(niveau,par,info,npas=1000,mode='N'):
 
 
 #================ tracer la matrice ========================================
-e,matrice_p = creat_matrice(2,par='b')
+#e,matrice_p = creat_matrice(2,par='b')
 #print(len(e))
 #print(matrice_p[76:90,25])
 #print(matrice_p.shape,matrice_p[0,541:543])
-ecri = ecrit_matrice(matrice_p,2,'b',16) 
-#fig1 = matrice_fig(matrice_p,50,100,e,'p',10)
+#ecri = ecrit_matrice(matrice_p,2,'b',16) 
+#fig1 = matrice_fig(matrice_p,2000,10000,e,'b',16)
 #for i in range(1003):
-  #  for j in range(901):
- #       matrice_p[i][j] = np.log(matrice_p[i][j]+1e-8)
-#fig2 = matrice_fig(matrice_p,200,1000,e,'b',v=16)
+   #for j in range(801):
+       #matrice_p[i][j] = np.log(matrice_p[i][j]+1e-8)
+#fig2 = matrice_fig(matrice_p,2000,10000,e,'b',v=16)
 #print(fig2)
 
 #============= tracer la proba à Ei=Ed ====================================== 
