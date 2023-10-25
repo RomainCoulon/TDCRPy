b43096926eb286a1ee019370c6d2bca52749847c
Jialin HU <jialin.hu@bipm.org>
2023-10-25 10:05:21 +0200

    matrice beta 200-2000k

--- tdcrpy/MCNP-MATRIX/matrice.py
+++ tdcrpy/MCNP-MATRIX/matrice.py
@@ -496,16 +496,16 @@ def find_info(niveau,par,info,npas=1000,mode='N'):
 
 
 #================ tracer la matrice ========================================
-#e,matrice_p = creat_matrice(0,par='b')
+#e,matrice_p = creat_matrice(1,par='b')
 #print(len(e))
 #print(matrice_p[76:90,25])
 #print(matrice_p.shape,matrice_p[0,541:543])
-#ecri = ecrit_matrice(matrice_p,2,'p',13) 
+#ecri = ecrit_matrice(matrice_p,1,'b',13) 
 #fig1 = matrice_fig(matrice_p,1,20,e,'b',13)
 # for i in range(1003):
-#     for j in range(200):
+#     for j in range(901):
 #         matrice_p[i][j] = np.log(matrice_p[i][j]+1e-8)
-# fig2 = matrice_fig(matrice_p,1,200,e,'b',v=10)
+# fig2 = matrice_fig(matrice_p,200,2000,e,'b',v=13)
 #print(fig2)
 
 #============= tracer la proba à Ei=Ed ====================================== 
